986ee81eb5ef9920e5c7e69d9df2773dfcd28fab
Tivain <70811407+Tivain-W@users.noreply.github.com>
2022-09-10 21:14:26 +0530

    Added AllServicesController.cs + a route for it
    Completed Part c) of the Registry project

---
+++ Registry/Controllers/AllServicesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Registry.Models;
+using System.Web;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Registry.Controllers
+{
+    [RoutePrefix("api/Registry")]
+    public class AllServicesController : ApiController
+    {
+        [Route("allServices")]
+        [HttpGet]
+        public List<PublishModel> GetAllServices()
+        {
+            string servicesFileLocation = HttpContext.Current.Server.MapPath("~/App_Data/datastore/publish.txt");
+
+            try
+            {
+                List<PublishModel> allServices = File.ReadLines(servicesFileLocation).Where(x => !string.IsNullOrWhiteSpace(x)).Select(s => JsonConvert.DeserializeObject<PublishModel>(s)).ToList();
+
+                return allServices;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
