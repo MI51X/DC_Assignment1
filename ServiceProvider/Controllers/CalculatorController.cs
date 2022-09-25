b7c5850fa95202d4d6ba1ab2fed1d6d0a9be79e0
Charuka Kaluarachchi <70794286+cdkaluarachchi@users.noreply.github.com>
2022-09-25 17:10:29 +0530

    Integrated ServiceProvider -> AuthStatusProvider with CalculatorController

--- ServiceProvider/Controllers/CalculatorController.cs
+++ ServiceProvider/Controllers/CalculatorController.cs
@@ -19,44 +19,48 @@ namespace ServiceProvider.Controllers
         [Route("addTwoNumbers/{num1}/{num2}")]
         [Route("addTwoNumbers")]
         [HttpGet]
-        public OutputJson AddTwoNumbers(int num1, int num2)
+        public IHttpActionResult AddTwoNumbers(int token, int num1, int num2)
         {
+            if (new AuthStatusProvider().AuthStatusCheck(token, out AuthStatusProvider.ResponseModel response) == false) { return Json(response);}
             OutputJson outputJson = new OutputJson();
             outputJson.output = num1 + num2;
-            return outputJson;
+            return Ok(outputJson);
         }
-
+        
         // GET: AddThreeNumbers
         [Route("addThreeNumbers/{num1}/{num2}/{num3}")]
         [Route("addThreeNumbers")]
         [HttpGet]
-        public OutputJson AddThreeNumbers(int num1, int num2, int num3)
+        public IHttpActionResult AddThreeNumbers(int token, int num1, int num2, int num3)
         {
+            if (new AuthStatusProvider().AuthStatusCheck(token, out AuthStatusProvider.ResponseModel response) == false) { return Json(response); }
             OutputJson outputJson = new OutputJson();
             outputJson.output = num1 + num2 + num3;
-            return outputJson;
+            return Ok(outputJson);
         }
-
+        
         // GET: MultiplyTwoNumbers
         [Route("multiplyTwoNumbers/{num1}/{num2}")]
         [Route("multiplyTwoNumbers")]
         [HttpGet]
-        public OutputJson MultiplyTwoNumbers(int num1, int num2)
+        public IHttpActionResult MultiplyTwoNumbers(int token, int num1, int num2)
         {
+            if (new AuthStatusProvider().AuthStatusCheck(token, out AuthStatusProvider.ResponseModel response) == false) { return Json(response); }
             OutputJson outputJson = new OutputJson();
             outputJson.output = num1 * num2;
-            return outputJson;
+            return Ok(outputJson);
         }
 
         // GET: MultiplyThreeNumbers
         [Route("multiplyThreeNumbers/{num1}/{num2}/{num3}")]
         [Route("multiplyThreeNumbers")]
         [HttpGet]
-        public OutputJson MultiplyThreeNumbers(int num1, int num2, int num3)
+        public IHttpActionResult MultiplyThreeNumbers(int token, int num1, int num2, int num3)
         {
+            if (new AuthStatusProvider().AuthStatusCheck(token, out AuthStatusProvider.ResponseModel response) == false) { return Json(response); }
             OutputJson outputJson = new OutputJson();
             outputJson.output = num1 * num2 * num3;
-            return outputJson;
+            return Ok(outputJson);
         }
 
     }
